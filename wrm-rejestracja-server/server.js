aa8d6e6b414aea75ae1ee6faa6c93be7c514db7b
m-warszawski <s223194@student.uek.krakow.pl>
2023-12-17 20:50:12 +0100

    Usuwanie i pobieranie rekordów

--- wrm-rejestracja-server/server.js
+++ wrm-rejestracja-server/server.js
@@ -28,80 +28,85 @@ const pacjenci = new Datastore({
     autoload: true
 });
 
-// --> Obsługa rejestracji na badania
+// --------------------------------> BADANIA <------------------------------------------
+// Dodanie rejestracji na badanie
 app.post("/add-test", function (req, res) {
-    doc = req.body;
-    badania.insert(doc, function (err, newDoc) {
+    badania.insert(req.body, function (err, newDoc) {
         if (err) {
-            console.error("Błąd podczas zapisywania badania:", err);
             res.send(err);
         } else {
-            console.log("Dodano badanie: " + newDoc._id)
-            console.log(newDoc)
             res.send("OK");
         }
     })
 })
 
+// Pobranie wszystkich rejestracji na badanie
+app.get('/get-all-tests', (req, res) => {
+    badania.find({ }, function (err, docs) {
+        res.send(JSON.stringify(docs, null, 5));
+    });
+});
+
+// Pobranie pojedyńczej rejestracji na badanie (na podstawie ID)
 app.get('/get-test', (req, res) => {
-    console.log(req.query)
-    res.send("Pod tym adresem można pobrać dane o rejestracji na badanie")
+    badania.findOne({ _id: req.query.id }, function (err, doc) {
+        res.send(JSON.stringify(doc, null, 5));
+    });
 });
 
+
+// Usunięcie rejestracji na badanie (na podstawie ID)
 app.post('/remove-test', (req, res) => {
-    id = req.body.id
-    badania.remove({ _id:  id}, { multi: true }, function (err, numRemoved) {
+    badania.remove({ _id:  req.body.id}, { multi: true }, function (err, numRemoved) {
         if (err) {
-            console.error("Błąd podczas usuwania:", err);
             res.send(false);
         } else {
-            console.log("Usunięto wpisów: " + numRemoved);
             res.send(JSON.stringify(numRemoved));
         }
     });
 });
 
-// --> Obsługa rejestracji na wizyty
+// --------------------------------> WIZYTY <------------------------------------------
+// Dodanie rejestracji na wizytę
 app.post("/add-visit", function (req, res) {
-    doc = req.body
-    wizyty.insert(doc, function (err, newDoc) {
+    wizyty.insert(req.body, function (err, newDoc) {
         if (err) {
-            console.error("Błąd podczas zapisywania wizyty:", err);
             res.send(err);
         } else {
-            console.log("Dodano wizytę: " + newDoc._id)
-            console.log(newDoc)
             res.send("OK");
         }
     })
 })
 
-app.get('/get-visit', (req, res) => {
-    console.log(req.query)
-    res.send("Pod tym adresem można pobrać dane o rejestracji na wizytę")
+// Pobranie wszystkich rejestracji na wizytę
+app.get('/get-all-visits', (req, res) => {
+    wizyty.find({ }, function (err, docs) {
+        res.send(JSON.stringify(docs, null, 5));
+    });
 });
 
+// Pobranie pojedyńczej rejestracji na wizytę (na podstawie ID)
+app.get('/get-visit', (req, res) => {
+    wizyty.findOne({ _id: req.query.id }, function (err, doc) {
+        res.send(JSON.stringify(doc, null, 5));
+    });
+});
+
+// Usunięcie rejestracji na wizytę (na podstawie ID)
 app.post('/remove-visit', (req, res) => {
-    id = req.body.id
-    wizyty.remove({ _id:  id}, { multi: true }, function (err, numRemoved) {
+    wizyty.remove({ _id:  req.body.id}, { multi: true }, function (err, numRemoved) {
         if (err) {
-            console.error("Błąd podczas usuwania:", err);
             res.send(false);
         } else {
-            console.log("Usunięto wpisów: " + numRemoved);
             res.send(JSON.stringify(numRemoved));
         }
     });
 });
 
-// --> Obsługa logowania
+// --------------------------------> LOGOWANIE <------------------------------------------
+// Sprawdzanie czy dane konto istnieje
 app.post("/login", function (req, res) {
-    const username = req.body.username;
-    const password = req.body.password;
-    console.log('Username:', username);
-    console.log('Password:', password);
-
-    pacjenci.count({ username: username, password: password},  function (err, count) {
+    pacjenci.count({ username: req.body.username, password: req.body.password},  function (err, count) {
         if(count === 1){
             res.send(true);
         }
@@ -111,10 +116,7 @@ app.post("/login", function (req, res) {
     });
 })
 
-// app.get('/logout', (req, res) => {
-//     // console.log(req.query)
-//     res.send("Pod tym adresem można wylogować się z systemu")
-// });
+// -------------------------------------------------------------------------------------
 
 // Nasłuchiwanie na porcie 3000
 app.listen(port, () => {
